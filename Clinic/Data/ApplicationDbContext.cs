041839c0b742de0a0e530b6362552681b517be65
mhamad98 <46487299+mhamad98@users.noreply.github.com>
2019-05-01 00:26:08 +0300

    rr

--- Clinic/Data/ApplicationDbContext.cs
+++ Clinic/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Clinic.Models;
 
 namespace Clinic.Data
 {
@@ -12,5 +13,6 @@ namespace Clinic.Data
             : base(options)
         {
         }
+        public DbSet<Clinic.Models.Doctor> Doctor { get; set; }
     }
 }
